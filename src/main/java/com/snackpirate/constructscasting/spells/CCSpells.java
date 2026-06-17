b2b1923bec9c2d3b5961d0d8f295cb295a92e031
Maf1sher <kubas307@gmail.com>
2026-06-17 19:17:10 +0200

    Migrate spell and school registration to NeoForge

--- src/main/java/com/snackpirate/constructscasting/spells/CCSpells.java
+++ src/main/java/com/snackpirate/constructscasting/spells/CCSpells.java
@@ -9,57 +9,62 @@ import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import io.redspace.ironsspellbooks.api.spells.SchoolType;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NewRegistryEvent;
+import net.neoforged.neoforge.registries.RegistryBuilder;
 
-import java.util.List;
+import java.util.function.Supplier;
 
 import static io.redspace.ironsspellbooks.api.registry.SpellRegistry.SPELL_REGISTRY_KEY;
 
 public class CCSpells {
 
-	public static final DeferredRegister<AbstractSpell> SPELLS = DeferredRegister.create(SPELL_REGISTRY_KEY, ConstructsCasting.MOD_ID);
-
+	public static final Registry<AbstractSpell> SPELL_REGISTRY = new RegistryBuilder<>(SPELL_REGISTRY_KEY).create();
+	private static final DeferredRegister<AbstractSpell> SPELLS = DeferredRegister.create(SPELL_REGISTRY_KEY, ConstructsCasting.MOD_ID);
 
 	public static void register(IEventBus eventBus) {
 		SPELLS.register(eventBus);
+		eventBus.addListener(CCSpells::registerCustomRegistries);
 		Attributes.ATTRIBUTES.register(eventBus);
 		Schools.SCHOOLS.register(eventBus);
 	}
 
-	public static final RegistryObject<AbstractSpell> FREEZE_SPELL = registerSpell(new FreezeSpell());
-	public static final RegistryObject<AbstractSpell> INVERT = registerSpell(new InvertSpell());
-//	public static final RegistryObject<AbstractSpell> SLIMEBALL_SPELL = registerSpell(new SlimeballSpell());
+	public static void registerCustomRegistries(NewRegistryEvent event) {
+		event.register(SPELL_REGISTRY);
+		event.register(Schools.SCHOOL_REGISTRY);
+	}
 
-	public static RegistryObject<AbstractSpell> registerSpell(AbstractSpell spell) {
+	public static final Supplier<AbstractSpell> FREEZE_SPELL = registerSpell(new FreezeSpell());
+	public static final Supplier<AbstractSpell> INVERT = registerSpell(new InvertSpell());
+
+	public static Supplier<AbstractSpell> registerSpell(AbstractSpell spell) {
 		return SPELLS.register(spell.getSpellName(), () -> spell);
 	}
 
-
-
-	@Mod.EventBusSubscriber(modid = ConstructsCasting.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+	@EventBusSubscriber(modid = ConstructsCasting.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 	public static class Attributes {
-		public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, ConstructsCasting.MOD_ID);
+		private static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(Registries.ATTRIBUTE, ConstructsCasting.MOD_ID);
 
-		public static final RegistryObject<Attribute> SLIME_POWER = newPowerAttribute("slime");
+		public static final DeferredHolder<Attribute, Attribute> SLIME_POWER = newPowerAttribute("slime");
+		public static final DeferredHolder<Attribute, Attribute> SLIME_RESIST = newResistanceAttribute("slime");
 
-		public static final RegistryObject<Attribute> SLIME_RESIST = newResistanceAttribute("slime");
-
-		private static RegistryObject<Attribute> newResistanceAttribute(String id) {
+		private static DeferredHolder<Attribute, Attribute> newResistanceAttribute(String id) {
 			return ATTRIBUTES.register(id + "_magic_resist", () -> (new MagicRangedAttribute("attribute.constructs_casting." + id + "_magic_resist", 1.0D, -100, 100).setSyncable(true)));
 		}
 
-		private static RegistryObject<Attribute> newPowerAttribute(String id) {
+		private static DeferredHolder<Attribute, Attribute> newPowerAttribute(String id) {
 			return ATTRIBUTES.register(id + "_spell_power", () -> (new MagicRangedAttribute("attribute.constructs_casting." + id + "_spell_power", 1.0D, -100, 100).setSyncable(true)));
 		}
 		@SubscribeEvent
@@ -69,26 +74,24 @@ public class CCSpells {
 
 	}
 
-
-
 	public static class Schools {
-		public static final DeferredRegister<SchoolType> SCHOOLS = DeferredRegister.create(SchoolRegistry.SCHOOL_REGISTRY_KEY, ConstructsCasting.MOD_ID);
+		private static final DeferredRegister<SchoolType> SCHOOLS = DeferredRegister.create(SchoolRegistry.SCHOOL_REGISTRY_KEY, ConstructsCasting.MOD_ID);
+		public static final Registry<SchoolType> SCHOOL_REGISTRY = new RegistryBuilder<>(SchoolRegistry.SCHOOL_REGISTRY_KEY).create();
 
 		public static final ResourceLocation SLIME_LOC = ConstructsCasting.id("slime");
-		public static final RegistryObject<SchoolType> SLIME = registerSchool(new SchoolType(SLIME_LOC,
+		public static final Supplier<SchoolType> SLIME = registerSchool(new SchoolType(SLIME_LOC,
 				CCItems.Tags.SLIME_FOCUS,
 				Component.translatable("school.constructs_casting.slime").withStyle(Style.EMPTY.withColor(0x119c3b)),
 				Attributes.SLIME_POWER,
 				Attributes.SLIME_RESIST,
-				() -> CCSounds.SLIME_CAST.get(),
+				CCSounds.SLIME_CAST,
 				CCDamageTypes.SLIME_MAGIC,
 				false,
 				true
 		));
 
-		private static RegistryObject<SchoolType> registerSchool(SchoolType schoolType) {
+		private static Supplier<SchoolType> registerSchool(SchoolType schoolType) {
 			return SCHOOLS.register(schoolType.getId().getPath(), () -> schoolType);
 		}
-
 	}
 }
