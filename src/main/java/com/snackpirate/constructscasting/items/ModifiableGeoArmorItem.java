aef167037c26c5e8162c594f21c8e84f6e3473ae
Maf1sher <kubas307@gmail.com>
2026-06-17 19:34:51 +0200

    Fix GeckoLib imports, conditions package, ModifierId references

--- src/main/java/com/snackpirate/constructscasting/items/ModifiableGeoArmorItem.java
+++ src/main/java/com/snackpirate/constructscasting/items/ModifiableGeoArmorItem.java
@@ -5,12 +5,12 @@ import net.minecraft.world.item.ArmorMaterial;
 import slimeknights.tconstruct.library.tools.definition.ToolDefinition;
 import slimeknights.tconstruct.library.tools.item.armor.ModifiableArmorItem;
 import software.bernie.geckolib.animatable.GeoItem;
-import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
-import software.bernie.geckolib.core.animation.AnimationState;
-import software.bernie.geckolib.core.animation.RawAnimation;
-import software.bernie.geckolib.core.object.PlayState;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class ModifiableGeoArmorItem extends ModifiableArmorItem implements GeoItem {
